5004b032b7f866c57d8561ef626686249c6fe931
juanlerch <juanlerc@gmail.com>
2015-06-13 09:07:37 -0300

    HTML in dsl

---
+++ XtremEngine/src/com/labs/xe/client/dto/XEATT_XDTO.java
@@ -0,0 +1,26 @@
+package com.labs.xe.client.dto;
+
+public class XEATT_XDTO implements XEIATT<XEIDTO> {
+	
+	XEIDTO value;
+	@Override
+	public XEIDTO getValue() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+	
+	
+	public void setValue(XEIDTO value) {
+		this.value = value;
+	}
+	
+	
+	
+@Override
+public String toString() {
+	return value.toString();
+}
+	
+	
+
+}
---
+++ XtremEngine/src/com/labs/xe/server/dsl/ui/HTML.java
@@ -0,0 +1,17 @@
+package com.labs.xe.server.dsl.ui;
+
+
+public class HTML extends Base {
+
+	
+	String html;
+	public HTML() {
+	
+	}
+
+	public void setHTML(String html){
+		this.html=html;
+	}
+	 	
+	
+}
